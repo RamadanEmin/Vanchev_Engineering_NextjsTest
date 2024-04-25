9bc22759abf3653d2db59f85010885dbb6199b72
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-25 17:56:59 +0300

    Added Cards information

---
+++ utils/constants.js
@@ -0,0 +1,23 @@
+export const cards = [
+    {
+        heading: "Brief",
+        text: "Complete **brief writing or simple guidance** on what to include, we've got you covered",
+        image: { src: "/icon/brief.png", alt: "Tools icon", position: "left" },
+        marginRight: 60,
+        href: "https://goodbrief.io/"
+    },
+    {
+        heading: "Search",
+        text: "In-depth agency search covering; **criteria matching**, door knocking and due dilligence vetting.",
+        image: { src: "/icon/search.png", alt: "Business icon", position: "left" },
+        marginRight: 30,
+        href: "#"
+    },
+    {
+        heading: "Pitch",
+        text: "Coperhensive **pitch management**, including comms, diary management and pitch hosting.",
+        image: { src: "/icon/pitch.png", alt: "Business icon", position: "left" },
+        marginRight: 0,
+        href: "#"
+    }
+];
